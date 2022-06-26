713a55b43061f548b8bcb15ca90408db6f9bc0e1
shlomi <shlomib2884@gmail.com>
2022-06-26 17:17:45 +0300

    new component button & link

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,7 +1,8 @@
+import Button_w140_w383_Link from "../common/Button w140-OR-w383 Link"
 
 function Test(){
     return <>
-    
+    <Button_w140_w383_Link href="jhjh" small={true} white={false}/>
     </>
 }
 
