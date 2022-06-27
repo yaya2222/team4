410ec2adcdcfbefb8bb90025f14c37a98c6e7288
Amichai <amichaicohen@gmail.com>
2022-06-27 13:04:49 +0300

    day 1

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,7 +1,10 @@
+import PlayPauseButton from "../common/PlayPauseButton/templates/componentTemplate"
 
 function Test(){
     return <>
-    
+     {/* <PlayPauseButton state={"+"}/> */}
+    {/* <PlayPauseButton state={"-"}/>  */}
+    <PlayPauseButton fullStop="a" />
     </>
 }
 
