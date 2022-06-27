18a039f5050160ef2c1e2546b9118ef84b2627ac
shlomi <shlomib2884@gmail.com>
2022-06-27 12:09:31 +0300

    add iconButton& homeButton admin componnents

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -2,7 +2,7 @@ import Button_w140_w383_Link from "../common/Button w140-OR-w383 Link"
 
 function Test(){
     return <>
-    <Button_w140_w383_Link href="jhjh" small={true} white={false}/>
+    <Button_w140_w383_Link  small={false} white={true}/>
     </>
 }
 
