194aa77d03e1974ad6eece7f8a7568bfb1929fe6
unknown <oklein01@gmail.com>
2022-06-27 13:01:59 +0300

    book

---
+++ src/components/commn/admin/homeButton/index.jsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom'
+import styles from './style.module.css'
+import iconStudent from './img/Vector.svg'
+import iconclass from './img/Group.svg'
+
+
+// Creator : team 3 -  yoni
+// for link send to=path
+// default iconclass. for iconstudent send icon=true
+//send  number=number
+
+function SelectButton(props) {
+    let text = props.icon ? "Students" : "Classrooms"
+    return <>
+        <Link className={styles.Link} to={props.to}>
+            <div className={styles.button}>
+                <div className={styles.main}>
+                    <img src={props.icon ? iconStudent : iconclass} className={styles.icon} />
+                    <div className={styles.title}>
+                        <h3 className={styles.h3}>1555{props.number}</h3>
+                        <div className={styles.text}>{text}</div>
+                    </div>
+                </div>
+            </div>
+        </Link>
+
+    </>
+}
+
+
+export default SelectButton
--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,7 +1,7 @@
-
+import SelectButton from '../commn/admin/homeButton/index'
 function Test(){
     return <>
-    
+    <SelectButton to="/" icon={true} />
     </>
 }
 
