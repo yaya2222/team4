f8745b973d36c4b9db9c2277ea95fdf3979f43e8
unknown <e.yanki267@gmail.com>
2022-06-28 16:12:57 +0300

    a

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,11 +1,13 @@
 import Pushup1B from "../../pages-order/trainReading/Pushup1B"
 import Questions from "../../pages-order/trainReading/Questions"
+import ReadingRate from "../../pages-order/levelExercise/ReadingRate/index"
 
 function Test(){
 
     
 return <>
-<Questions />
+{/* <Questions /> */}
+<ReadingRate/>
 </>
 }
 
--- src/pages-order/levelExercise/ReadingRate/index.jsx
+++ src/pages-order/levelExercise/ReadingRate/index.jsx
@@ -1,11 +1,23 @@
 import styles from './style.module.css'
-
+import DivCompicated from '../../../components/common/DivCompicated'
+import Button_w140_w383_Link from'../../../components/common/Button w140-OR-w383 Link'
 // Creator : Team A - Shahar
-function Temp(){
+function Rate() {
     return <>
-    
+        <DivCompicated classAll={styles.bigDiv} classInternal={styles.div} isLeft>
+            <p>How was that reading rate?</p>
+            <div className={styles.divAll}>
+                <div className={styles.btn}>
+                    <Button_w140_w383_Link text="just Right"/>
+                </div>
+                <div className={styles.btn}>
+                    <Button_w140_w383_Link white={true} text="Too Fast" />
+                </div>
+                <div className={styles.btn}>
+                    <Button_w140_w383_Link white={true} text="Too Slow" />
+                </div>
+            </div>
+        </DivCompicated>
     </>
 }
-
-
-export default Temp
+export default Rate
