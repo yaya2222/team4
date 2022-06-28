f0abbeae129d313dc718738510d4b5772b075756
shlomi <shlomib2884@gmail.com>
2022-06-28 14:01:03 +0300

    merge with yossi

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { SignUp } from "../../pages-order/signup";
 
 export default function(){
@@ -6,13 +5,3 @@ export default function(){
     <SignUp/>
     </>
 }
-=======
-
-import ConnectTeacher from '../../pages-order/signup/ConnectTeacher'
-function Test(){
-    return <>
-    <ConnectTeacher />
-    </>
-}
-export default Test
->>>>>>> 2d505d423789b5a497411f1f2ae0f1a510e9ece7
