c95f1d87b7106f17175a78a0866801963a301c95
shlomi <shlomib2884@gmail.com>
2022-06-28 14:03:44 +0300

    merge with yossi

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { SignUp } from "../../pages-order/signup";
 
 export default function(){
@@ -6,15 +5,3 @@ export default function(){
     <SignUp/>
     </>
 }
-=======
-import ForgotPassword from "../../pages-order/signup/ForgotPassword"
-
-function Test(){
-    return <>
-<ForgotPassword/>
-    </>
-}
-
-
-export default Test
->>>>>>> 1e7bfe587fd41da43b78a7dd8db5f8feaadaa366
