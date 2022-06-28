fd4c49fa88ba03312b4b80a8da4ea81a3f0181ec
netanel machrabi <netanel61096@gmail.com>
2022-06-28 13:06:51 +0300

    sign in

---
+++ src/components/test/Test.js
@@ -0,0 +1,6 @@
+import SignIn from "../../pages-order/signup/SignIn";
+export default function Test(){
+return <>
+<SignIn />
+</>
+}
--- src/pages-order/signup/SignIn/index.jsx
+++ src/pages-order/signup/SignIn/index.jsx
@@ -1,11 +1,27 @@
 import styles from './style.module.css'
+import InputTypes from '../../../components/common/inputTypes'
+import Button_w140_w383_Link from '../../../components/common/Button w140-OR-w383 Link'
+import Beackground from '../../../components/common/SignWithGoogle/Beackground'
 
+import { Link }from "react-router-dom"
 // Creator : Team A - Shahar
-function Temp(){
-    return <>
-    
-    </>
+function SignIn(props){
+    return <div className={styles.all}>
+    <h2>welcome back!</h2>
+    <p>Don't have an account?<Link to="signup">sign up</Link></p>
+    <form>
+        <InputTypes type="text"/>
+        <InputTypes type="password"/>
+
+        <div className={styles.button}>
+            <Button_w140_w383_Link text="sign in"/>
+            </div>
+
+
+    </form>
+    <Beackground />
+    </div>
 }
 
 
-export default Temp
+export default SignIn
