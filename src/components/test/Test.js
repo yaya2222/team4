b2ff35a66605c57743fd4d0bc61111c1c2ea2a1c
יעקב גולדמן <gyaakov2000@gmail.com>
2022-06-28 13:37:38 +0300

    merge with yaakov_elos

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Pushup1B from "../../pages-order/trainReading/Pushup1B"
 import Questions from "../../pages-order/trainReading/Questions"
 
@@ -8,16 +7,6 @@ function Test(){
 return <>
 <Questions />
 </>
-=======
-import BookDetails from "../../pages-order/trainReading/BookDetails"
-import InputTypes from "../common/inputTypes"
-
-
-function Test(){
-    return <>
-    <BookDetails />
-    </>
->>>>>>> 852215434757d1a06825dcd1e547a941e68bbbbe
 }
 
 export default Test
--- src/pages-order/trainReading/BookDetails/index.jsx
+++ src/pages-order/trainReading/BookDetails/index.jsx
@@ -2,11 +2,7 @@ import styles from './style.module.css'
 import InputTypes from "../../../components/common/inputTypes"
 import Button_w140_OR_w383_Link from  "../../../components/common/Button w140-OR-w383 Link" 
 
-<<<<<<< HEAD
 // Creator : Team 4 - all
-=======
-// Creator : Team A - Shahar
->>>>>>> 852215434757d1a06825dcd1e547a941e68bbbbe
 function BookDetails(){
     return <>
     <InputTypes description="What Book are you going to read?" type="text"/>
