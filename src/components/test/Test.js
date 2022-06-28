852215434757d1a06825dcd1e547a941e68bbbbe
Amichai <amichaicohen@gmail.com>
2022-06-28 12:56:13 +0300

    day2

---
+++ src/components/test/Test.js
@@ -0,0 +1,11 @@
+import BookDetails from "../../pages-order/trainReading/BookDetails"
+import InputTypes from "../common/inputTypes"
+
+
+function Test(){
+    return <>
+    <BookDetails />
+    </>
+}
+
+export default Test
--- src/pages-order/trainReading/BookDetails/index.jsx
+++ src/pages-order/trainReading/BookDetails/index.jsx
@@ -1,11 +1,15 @@
 import styles from './style.module.css'
+import InputTypes from "../../../components/common/inputTypes"
+import Button_w140_OR_w383_Link from  "../../../components/common/Button w140-OR-w383 Link" 
 
 // Creator : Team A - Shahar
-function Temp(){
+function BookDetails(){
     return <>
-    
+    <InputTypes description="What Book are you going to read?" type="text"/>
+    <InputTypes description="Genre?" type="text"/>
+    <div className={styles.button}><Button_w140_OR_w383_Link href="/instructions"/></div>
     </>
 }
 
 
-export default Temp
+export default BookDetails
