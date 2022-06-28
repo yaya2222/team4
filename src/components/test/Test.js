ac8e7aac6158e6540db2cdd9beb16b842ec02e43
shlomi <shlomib2884@gmail.com>
2022-06-28 14:04:46 +0300

    merge with netanel

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { SignUp } from "../../pages-order/signup";
 
 export default function(){
@@ -6,11 +5,3 @@ export default function(){
     <SignUp/>
     </>
 }
-=======
-import SignIn from "../../pages-order/signup/SignIn";
-export default function Test(){
-return <>
-<SignIn />
-</>
-}
->>>>>>> fd4c49fa88ba03312b4b80a8da4ea81a3f0181ec
