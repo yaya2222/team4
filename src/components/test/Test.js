db409950fe2500f9c004b957f5df062d49bef2f4
Netanel <netanell1235@gmail.com>
2022-06-28 13:29:04 +0300

    Update version4

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -2,7 +2,7 @@ import StartButton from "../common/ButtonStart"
 import Movies from "../common/Movies"
 import PopUp from "../common/PopUp"
 import styles from "./styles.css"
-import dv from "../common/Movies/a.mov"
+// import dv from "../common/Movies/a.mov"
 import ConvergesExercise from "../../pages-order/trainFocus/ConvergesExercise"
 import ResultSummary from "../../pages-order/trainFocus/ResultSummary"
 import DivCompicated from "../common/DivCompicated"
@@ -12,7 +12,7 @@ function Test() {
         {/* <ConvergesExercise /> */}
         {/* <ResultSummary /> */}
         {/* <PopUp question="do you" /> */}
-        <Movies video={dv} />
+        <Movies />
     </>
 }
 
