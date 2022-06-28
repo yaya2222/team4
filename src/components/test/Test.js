3101746e06c7ae617d22e28aff97700323bf5101
Amichai <amichaicohen@gmail.com>
2022-06-28 16:18:13 +0300

    day3

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,10 +1,12 @@
-import BookDetails from "../../pages-order/trainReading/BookDetails"
-import InputTypes from "../common/inputTypes"
+import ResultSummaryL from "../../pages-order/levelExercise/ResultSummaryL"
+// import BookDetails from "../../pages-order/trainReading/BookDetails"
+// import InputTypes from "../common/inputTypes"
+import LevelExercise from "../../pages-order/levelExercise/LevelExercise"
 
 
 function Test(){
     return <>
-    <BookDetails />
+    <LevelExercise/>
     </>
 }
 
