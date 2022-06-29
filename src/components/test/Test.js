cbda9ba80fe8d989a42b04bd04a897ba61d8a5b6
יעקב גולדמן <gyaakov2000@gmail.com>
2022-06-29 09:44:55 +0300

    a

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,49 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import ResultSummaryL from "../../pages-order/levelExercise/ResultSummaryL"
-// import BookDetails from "../../pages-order/trainReading/BookDetails"
-// import InputTypes from "../common/inputTypes"
-import LevelExercise from "../../pages-order/levelExercise/LevelExercise"
-=======
-import Pushup1B from "../../pages-order/trainReading/Pushup1B"
-import Questions from "../../pages-order/trainReading/Questions"
-import ReadingRate from "../../pages-order/levelExercise/ReadingRate/index"
->>>>>>> aff8460b7864410967236d1657e0ca6a70a8e43c
 
->>>>>>> d61a7e973b05227e8d641084b090beb7bbdf9b07
 
-import Music from "../common/Music/index"
-import TimerWithAll from "../common/TimerWithAll"
-import B from  "../common/Button w140-OR-w383 Link"
-import SpeedLimit from "../common/SpeedLimit"
 import Pushup1B from "../../pages-order/trainReading/Pushup1B"
 function Test(){
-<<<<<<< HEAD
-
-    
-return <>
-<<<<<<< HEAD
-{/* <TimerWithAll clockTime={!true}> */}
-    {/* <B/> */}
-    {/* <SpeedLimit numberLPM={10} /> */}
-{/* </TimerWithAll> */}
-{/* <Music /> */}
-<Pushup1B />
-=======
-<<<<<<< HEAD
-{/* <Questions /> */}
-<ReadingRate/>
-=======
-{/* <T /> */}
->>>>>>> 4afb2307ad4231d93675c220ede10816055efaf2
->>>>>>> aff8460b7864410967236d1657e0ca6a70a8e43c
-</>
-=======
     return <>
-    <LevelExercise/>
+    <Pushup1B/>
     </>
->>>>>>> d61a7e973b05227e8d641084b090beb7bbdf9b07
 }
 
 export default Test
--- src/pages-order/levelExercise/LevelExercise/index.jsx
+++ src/pages-order/levelExercise/LevelExercise/index.jsx
@@ -1,17 +1,9 @@
 // import styles from './style.module.css'
-<<<<<<< HEAD
 // import Timer from '../../../components/common/Timer'
 // // Creator : Team 4 - amichai
 // function LevelExercise(){
 //     return <>
 //     <Timer />
-=======
-// // import Timer from '../../../components/common/Timer'
-// // Creator : Team 4 - yaakov goldman
-// function TemLevelExercisep(){
-//     return <>
-//     {/* <Timer /> */}
->>>>>>> 4afb2307ad4231d93675c220ede10816055efaf2
 //     </>
 // }
 
