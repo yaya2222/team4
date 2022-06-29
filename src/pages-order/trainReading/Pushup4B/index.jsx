98d181b3e71cd84e78fd36455f2b5aa6de454d96
יעקב גולדמן <gyaakov2000@gmail.com>
2022-06-29 09:25:59 +0300

    git

--- src/pages-order/trainReading/Pushup4B/index.jsx
+++ src/pages-order/trainReading/Pushup4B/index.jsx
@@ -1,11 +1,15 @@
+import Button_w140_w383_Link from '../../../components/common/Button w140-OR-w383 Link'
+import TimerWithAll from '../../../components/common/TimerWithAll'
 import styles from './style.module.css'
 
-// Creator : Team A - Shahar
-function Temp(){
+// Creator : Team 4 - yaakov goldman
+function Pushup4B(){
     return <>
-    
+    <TimerWithAll>
+        <Button_w140_w383_Link text="Stop" href="/a" />
+    </TimerWithAll>
     </>
 }
 
 
-export default Temp
+export default Pushup4B
